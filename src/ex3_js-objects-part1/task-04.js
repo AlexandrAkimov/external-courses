43677348737766358edd55c00154acf38905f355
AlexandrAkimov <46198243+AlexandrAkimov@users.noreply.github.com>
2019-03-24 14:38:20 +0300

    Delete task-04.js

--- src/ex3_js-objects-part1/task-04.js
+++
@@ -1,9 +0,0 @@
-function addPropInObject(stringArg, obj) {
-		if (obj.hasOwnProperty(stringArg)){
-			return obj;
-		}
-		var newObj = obj;
-		newObj[stringArg] = 'new';
-		return newObj;
-}
-module.exports = addPropInObject;
