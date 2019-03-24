121796663810d45c1bacba2ac7d53ee6cc084635
AlexandrAkimov <46198243+AlexandrAkimov@users.noreply.github.com>
2019-03-24 14:37:56 +0300

    Delete task-02.js

--- src/ex3_js-objects-part1/task-02.js
+++
@@ -1,7 +0,0 @@
-function displayPropAndValue(obj) {
-	for(key in obj){
-		if(obj.hasOwnProperty(key)){console.log("ключ: " + key + ", значение: " + obj[key]);
-		}
-	}
-}
-module.exports = displayPropAndValue;
